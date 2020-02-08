0a5a2a55faab603cc478624bb1a66f1dcb276d16
0xMilk <1144502582@qq.com>
2020-02-09 00:22:41 +0800

    安装babel插件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
+    <test></test>
   </div>
 </template>
 
+
+<script>
+import Test from "./components/Test";
+  export default {
+    name: 'App',
+    components: {
+      Test
+    }
+  }
+</script>
 <style>
 
 </style>
