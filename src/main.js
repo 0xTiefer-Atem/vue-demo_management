b014fb64908a761867798afcf2fd16291ba2f31a
0xMilk <1144502582@qq.com>
2020-02-09 11:30:49 +0800

    添加全屏样式

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router/index'
 
+//导入全局样式表
+import './assets/css/global.css'
+
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
