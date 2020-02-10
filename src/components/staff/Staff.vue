1914355e9bdce26daa8274a1f0c214392b09b354
0xMilk <1144502582@qq.com>
2020-02-11 00:22:37 +0800

    完成添加员工对话框的渲染

--- src/components/staff/Staff.vue
+++ src/components/staff/Staff.vue
@@ -4,12 +4,203 @@
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item>排班日程</el-breadcrumb-item>
     </el-breadcrumb>
+<!--    卡片视图-->
+    <el-card>
+<!--      添加角色按钮区-->
+      <el-row>
+        <el-col>
+          <el-button type="primary" @click="dialogVisible = true">添加职工</el-button>
+        </el-col>
+      </el-row>
+
+<!--      角色列表区域-->
+      <el-table :data="staffList" border strip>
+<!--        添加索引列-->
+        <el-table-column type="index" label="序列" width="50px"></el-table-column>
+        <el-table-column label="员工编号" prop="staffId" ></el-table-column>
+        <el-table-column label="员工名称" prop="staffName" width="80px"></el-table-column>
+        <el-table-column label="性别" prop="staffSex" width="70px"></el-table-column>
+        <el-table-column label="联系方式" prop="staffTel"></el-table-column>
+        <el-table-column label="员工职称" prop="staffPos" width="60px"></el-table-column>
+        <el-table-column label="入职时间" prop="staffEntry"></el-table-column>
+        <el-table-column label="操作" width="210px">
+          <template slot-scope="scope">
+            <el-button type="primary" size="mini" icon="el-icon-edit">编辑信息</el-button>
+            <el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!--    添加职工-->
+      <el-dialog
+              title="添加职员"
+              :visible.sync="dialogVisible"
+              width="50%"
+              :label-position="'left'"
+              @close="cancelAdd">
+        <!--      内容主体区-->
+        <el-form :model="addForm"
+                 :inline="true"
+                 :rules="staffFormRules"
+                 ref="staffFormRef">
+              <el-form-item label="职员编号" prop="staffId">
+                <el-input v-model="addForm.staffId"></el-input>
+              </el-form-item>
+
+              <el-form-item  label="职员名称" prop="staffName">
+                <el-input v-model="addForm.staffName"></el-input>
+              </el-form-item>
+          <br>
+              <el-form-item label="职员性别" prop="staffSex">
+                <el-radio-group v-model="addForm.staffSex">
+                  <el-radio-button label="男"></el-radio-button>
+                  <el-radio-button label="女"></el-radio-button>
+                </el-radio-group>
+              </el-form-item>
+          <br>
+              <el-form-item label="联系方式" prop="staffTel">
+                <el-input v-model="addForm.staffTel"></el-input>
+              </el-form-item>
+              <el-form-item label="职员职位" prop="staffPos">
+                <el-input v-model="addForm.staffPos"></el-input>
+              </el-form-item>
+              <el-form-item label="入职时间" prop="staffEntry">
+                <el-date-picker
+                        v-model="addForm.staffEntry"
+                        format="yyyy-MM-d"
+                        value-format="yyyy-MM-dd"
+                        placeholder="选择时间">
+                </el-date-picker>
+              </el-form-item>
+        </el-form>
+        <!--      底部区域-->
+<!--        <span slot="footer">-->
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addStaff">确 定</el-button>
+<!--      </span>-->
+      </el-dialog>
+    </el-card>
+
+
   </div>
 </template>
 
 <script>
   export default {
-    name: "Staff"
+    name: "Staff",
+    data() {
+      //验证职员电话
+      let checkTel = (rule, value, callback) => {
+        const mobileReg = /^1[3|4|5|7|8][0-9]{9}$/
+        if(mobileReg.test(value)){
+          return callback();
+        }
+        callback(new Error("请输入正确的号"))
+      };
+      return {
+        staffList: [
+          {
+            staffId: '123124',
+            staffName: 'zcx',
+            staffSex: '男',
+            staffPos: '专家',
+            staffTel: '15810227777',
+            staffEntry: '2020-1-20 8:00:00'
+          },
+          {
+            staffId: '123124',
+            staffName: 'zcx',
+            staffSex: '男',
+            staffPos: '专家',
+            staffTel: '15810227777',
+            staffEntry: '2020-1-20 8:00:00'
+          },
+          {
+            staffId: '123124',
+            staffName: 'zcx',
+            staffSex: '男',
+            staffPos: '专家',
+            staffTel: '15810227777',
+            staffEntry: '2020-1-20 8:00:00'
+          },
+          {
+            staffId: '123124',
+            staffName: 'zcx',
+            staffSex: '男',
+            staffPos: '专家',
+            staffTel: '15810227777',
+            staffEntry: '2020-1-20 8:00:00'
+          },
+          {
+            staffId: '123124',
+            staffName: 'zcx',
+            staffSex: '男',
+            staffPos: '专家',
+            staffTel: '15810227777',
+            staffEntry: '2020-1-20 8:00:00'
+          },
+          {
+            staffId: '123124',
+            staffName: 'zcx',
+            staffSex: '男',
+            staffPos: '专家',
+            staffTel: '15810227777',
+            staffEntry: '2020-1-20 8:00:00'
+          },
+          {
+            staffId: '123124',
+            staffName: 'zcx',
+            staffSex: '男',
+            staffPos: '专家',
+            staffTel: '15810227777',
+            staffEntry: '2020-1-20 8:00:00'
+          }
+        ],
+        dialogVisible: false,
+        //添加数据的表单对象
+        addForm: {
+          staffId: '',
+          staffName: '',
+          staffSex: '',
+          staffPos: '',
+          staffTel: '',
+          staffEntry: ''
+        },
+        //添加表单的验证规则对象
+        staffFormRules: {
+          staffId: [
+            { required: true, message: '请输入职员编号', trigger: 'blur' },
+            { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+          ],
+          staffName: [
+            { required: true, message: '请输入职员名称', trigger: 'blur' },
+            { min: 3, max: 6, message: '长度在 3 到 6 个字符', trigger: 'blur' }
+          ],
+          staffSex: [
+            { required: true, message: '选择职员性别', trigger: 'blur' },
+          ],
+          staffTel: [
+            { required: true, message: '请输入号码', trigger: 'blur' },
+            { validator: checkTel, trigger: 'blur' }
+          ],
+          staffPos: [
+            { required: true, message: '请输入职位', trigger: 'blur' },
+          ],
+          staffEntry: [
+            { required: true, message: '请选择时间', trigger: 'blur' },
+          ]
+        }
+      }
+    },
+    methods: {
+      cancelAdd() {
+        this.dialogVisible = false;
+        this.$refs.staffFormRef.resetFields();
+      },
+      addStaff() {
+        console.log(this.addForm);
+      }
+    }
   }
 </script>
 
