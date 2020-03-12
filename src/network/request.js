c1116a659d985fff89f09f3b497d4f670065f6aa
0xMilk <1144502582@qq.com>
2020-03-12 19:10:11 +0800

    修改ip地址

--- src/network/request.js
+++ src/network/request.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 export function request(config) {
   //1. 创建axios实例
   const instance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://47.107.64.157:8080',
     // timeout: 5000,
     crossDomain:true
 
