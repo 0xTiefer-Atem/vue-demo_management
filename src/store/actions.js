67e423107bd24096a713d2e80d0a5d15901e41cd
0xMilk <1144502582@qq.com>
2020-04-20 23:23:13 +0800

    删除无用软件

--- src/store/actions.js
+++
@@ -1,9 +0,0 @@
-// export default {
-//   //context 就相当于store 对象
-//   aUpdateSate(context, payload) {
-//     setTimeout(() => {
-//       context.commit('');
-//       console.log(payload);
-//     }, 1000)
-//   }
-// }
